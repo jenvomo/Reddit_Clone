7f51251b54807e39a667da931ed44bcff98db1ab
jenkennedy <kennifer09@gmail.com>
2018-09-18 10:40:37 -0700

    Fix parent id assignment for comments

--- reddit_clone/app/controllers/comments_controller.rb
+++ reddit_clone/app/controllers/comments_controller.rb
@@ -21,6 +21,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:body,:post_id)
+    params.require(:comment).permit(:body,:post_id, :parent_comment_id)
   end
 end
